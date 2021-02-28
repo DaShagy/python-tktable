8678950478ee6692b779d8316bcc49a78ad5c42f
Juan José Abuin <juanj.abuin@gmail.com>
2021-02-27 23:41:13 -0300

    Insert row/col functions

--- tktable/__init__.py
+++ tktable/__init__.py
@@ -1,6 +1,5 @@
 __version__ = '0.1.0'
 
-
 from tkinter import *
 import numbers
 
@@ -18,21 +17,24 @@ class Table():
         self.selected_line = None
 
         self.bind_mouse_button()
-        self.create_cells()
+        self.create_table()
 
     def __str__(self) -> str:
-        return f"Rows: {self._row_number}, Cols: {self._col_number}"
+        return f"Rows: {self._row_number}, Cols: {self._col_number}\n{self.cells}"
 
     def bind_mouse_button(self):
         self._master.master.bind('<Button-1>', self.select_cell)
         self._master.master.bind('<Double-Button-1>', self.select_row)
         self._master.master.bind('<Triple-Button-1>', self.select_col)
 
-    def create_cells(self):
-        for row in range(self._col_number):
-            for col in range(self._row_number):
-                cell = Cell(self._master, col, row)
-                self.cells.append((cell, col, row))
+    def create_cell(self, posx, posy):
+        cell = Cell(self._master, posx, posy)
+        self.cells.append((cell, posx, posy))
+
+    def create_table(self):
+        for row in range(self._row_number):
+            for col in range(self._col_number):
+                self.create_cell(col, row)
 
     def get_cell(self, row, col):
         return [t[0] for _, t in enumerate(self.cells) if t[1]==col and t[2]==row].pop()
@@ -131,36 +133,51 @@ class Table():
     #TODO: Simplefy and generalize 'insert_headers'
     #############################
 
-    def insert_headers(self, data):
-        headers = [*data.keys()]
-        print(headers)
-        header_row = self.get_row(0, 0)
-        for cell, key in zip(header_row.get_cells(), headers):
-            cell.set_value(key)
+    #def insert_headers(self, data):
+    #    headers = [*data.keys()]
+    #    print(headers)
+    #    header_row = self.get_row(0, 0)
+    #    for cell, key in zip(header_row.get_cells(), headers):
+    #        cell.set_value(key)
 
     ##############################
-    '''
-
-    def insert_row(self):
-        new_row = Row(self._master, len(self.cols), self._row_number)
-        self.rows.append(new_row)
-        for col, cell in zip(self.cols, new_row.get_cells()):
-            col.get_cells().append(cell)
-        self._row_number += 1
-
-
-    #TODO: Implement insert columns
-
     
 
-    def insert_col(self):
-        new_col = Column(self._master, len(self.rows), self._col_number)
-        self.cols.append(new_col)
-        for row, cell in zip(self.rows, new_col.get_cells()):
-            row.get_cells().append(cell)
-        self._col_number += 1
+    def insert_cells(self, line, type="ROW"):
+        try:
+            if type == "ROW":
+                if 0 <= line < self._row_number:
+                    for (cell, col, row) in self.cells:
+                        i = self.cells.index((cell, col, row))
+                        if line <= row:
+                            _, y = cell.get_pos()
+                            cell.set_pos(col, y+1)
+                            self.cells[i] = (cell, col, y+1)
+                    for col in range(self._col_number):
+                        self.create_cell(col, line)
+                        self.get_cell(line, col).set_value(f"NEW ROW {line}")  
 
-    '''
+            if type == "COL":
+                if 0 <= line < self._col_number:
+                    for (cell, col, row) in self.cells:
+                        i = self.cells.index((cell, col, row))
+                        if line <= col:
+                            x, _ = cell.get_pos()
+                            cell.set_pos(x+1, row)
+                            self.cells[i] = (cell, x+1, row)
+                    for row in range(self._row_number):
+                        self.create_cell(line, row)
+                        self.get_cell(row, line).set_value(f"NEW COL {line}")                 
+        except:
+            pass
+
+    def insert_row(self, pos):
+        self.insert_cells(pos, type="ROW")
+        self._row_number += 1
+
+    def insert_col(self, pos):
+        self.insert_cells(pos, type="COL")
+        self._col_number += 1
 
 class Cell(Entry):
     def __init__(self, master, posx=0, posy=0):
@@ -189,8 +206,13 @@ class Cell(Entry):
     def get_value(self):
         return self._value.get()
 
+    def set_pos(self, posx, posy):
+        self._pos = (posx, posy)
+        self.grid(column=posx, row=posy)
+
     def get_pos(self):
         return (self._pos)
+    
 
 class Cell_Line():
     def __init__(self, master, cells):
@@ -227,10 +249,14 @@ def _test():
     root = Tk()
     root.title("Test")
     frame = Frame(root, padx=10, pady=10)
-    table = Table(frame)
-    #table.insert_row()
-    #table.insert_col()
-    #table.insert_row()
+    table = Table(frame, 3, 4)
+    #print(table)
+    table.insert_row(0)
+    #print(table)
+    table.insert_col(1)
+    #print(table)
+    table.insert_row(3)
+    #print(table)
     frame.pack()
 
     
